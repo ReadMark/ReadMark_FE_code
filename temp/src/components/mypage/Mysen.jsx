3aa239ccc4c9d135c734304071bd9d694f52d39c
이태연 <h2ymb5@gmail.com>
2025-09-18 11:51:44 +0900

    class

--- temp/src/components/mypage/Mysen.jsx
+++ temp/src/components/mypage/Mysen.jsx
@@ -3,17 +3,17 @@ import Bookimg from './bookImg.jpg'
 function Mysen(){
     return(
         <>
-        <div class="my-fa-page-each-container ma-fa-moon-container">      
-                <div><img class="main__book-img my-fa-book-img" src={Bookimg} alt="" /></div>
-                <div class="my-fa-page_books-texts">
-                  <div class="my-fa-page-book-title my-fa-page-moon-title">트로피컬 나이트</div>
-                  <div class="my-fa-page-book-text-center">
-                    <div class="my-fa-page-moon-text page">p105</div>
-                    <div class="my-fa-page-moon-text">이건 확신이야. 내 애정이, 내 목소리가 
+        <div className="my-fa-page-each-container ma-fa-moon-container">      
+                <div><img className="main__book-img my-fa-book-img" src={Bookimg} alt="" /></div>
+                <div className="my-fa-page_books-texts">
+                  <div className="my-fa-page-book-title my-fa-page-moon-title">트로피컬 나이트</div>
+                  <div className="my-fa-page-book-text-center">
+                    <div className="my-fa-page-moon-text page">p105</div>
+                    <div className="my-fa-page-moon-text">이건 확신이야. 내 애정이, 내 목소리가 
                     너에게 어떤 방식으로든 닿을 거라고 믿어.</div>
                   </div>
                   
-                  <div class="my-fa-page-moon-date">저장한 날짜: 2025-07-15</div>
+                  <div className="my-fa-page-moon-date">저장한 날짜: 2025-07-15</div>
                 </div>
               </div>
         </>
